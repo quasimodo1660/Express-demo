90107ff18c2a653015c23cd06f75368395362271
Quasimodo <quasimodo1660@gmail.com>
2018-02-08 14:53:44 -0800

    Add session and display the session in view

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 var express = require("express");
 var app = express();
+app.set('views',__dirname + '/views');
+app.set('view engine','ejs');
+var session = require('express-session');
+app.use(session({secret:'sbfrank'}));
+
+
+session.times=0;
+
 
 app.get('/',function(request,response){
-    response.send('<h1>'+request.headers['user-agent']+'</h1>');
+    if (session.times!=undefined){
+        session.times+=1;
+    }
+    else{
+        session.times=1;
+    }
+    response.render('counter',{times:session.times});
+})
+
+app.get('/add2',function(req,res){
+    session.times+=1;
+    res.redirect('/')
+})
+
+app.get('/reset',function(req,res){
+    session.times=undefined;
+    res.redirect('/')
 })
 
 app.use(express.static(__dirname + '/static'));
