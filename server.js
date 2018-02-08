bc367c6a51bc19bac53a47a23b2624d99d91c55c
Quasimodo <quasimodo1660@gmail.com>
2018-02-07 17:44:42 -0800

    add static html and css files

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ var app = express();
 
 app.get('/',function(request,response){
     response.send('<h1>'+request.headers['user-agent']+'</h1>');
-}).listen(6789,function(){
+})
+
+app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static/css'));
+console.log(__dirname);
+
+app.listen(6789,function(){
     console.log('Listening on port 6789');
 })
 
