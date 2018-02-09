25f184073537bc76cbf97a5ef4c4296865f6858e
Quasimodo <quasimodo1660@gmail.com>
2018-02-09 08:54:54 -0800

    test two server

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
+
 var express = require("express");
 var app = express();
 var path = require('path');
+
+//***********Headle request post**************/
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+//*************Templates**********************/
 app.set('views',__dirname + '/views');
 app.set('view engine','ejs');
+//**session */
 var session = require('express-session');
 app.use(session({secret:'sbfrank'}));
+//********connect to sqlite database***********/
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('/Users/Ben/Desktop/FoodTruck/db.sqlite3',(err)=>{
+    if(err){
+        console.error(err.message);
+    }
+    console.log('Connected to the FD database.')
+})
+
+let sql='SELECT DISTINCT username FROM auth_user';
+db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row.username);
+    });
+  });
+
+
+
+db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+
+
 
 
 session.times=0;
@@ -33,8 +67,34 @@ app.get('/reset',function(req,res){
 })
 
 app.post('/survey',function(req,res){
-    console.log(req.body);
-    res.render('result',{user:{name:req.body.Name,loc:req.body.location,lan:req.body.lang,de:req.body.des}})
+    var ts = new Date().toISOString().slice(0, 19).replace('T', ' ')+".000000";
+    console.log("INSERT INTO 'lunchbox_tag' (name, category_id) VALUES ("+req.body.Name+','+1+')');
+    console.log(ts);
+    console.log(req.body.Name);
+    let db = new sqlite3.Database('/Users/Ben/Desktop/FoodTruck/db.sqlite3',(err)=>{
+        if(err){
+            console.error(err.message);
+        }
+        console.log('Connected to the FD database.')
+    })
+    
+    db.run("INSERT INTO 'lunchbox_tag' (name, category_id) VALUES ("+'"'+req.body.Name+'",'+1+')', function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+      });
+    
+    
+    db.close((err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log('Close the database connection.');
+      });
+
+    res.redirect('http://localhost:8000/result')
 })
 
 
